98e499e110c3501e135a44baa1521b06a74f7e4c
Mankowski92 <mankowski92@gmail.com>
2021-05-22 14:25:38 +0200

    logic and fetching moved from provider to PokePedia component

--- src/components/PokePedia/PokePedia.js
+++ src/components/PokePedia/PokePedia.js
@@ -1,15 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyledGetPokemonButton, StyledPokemonContainer, StyledGetPokemonWrapper } from './PokePedia.styles';
 import { PokemonsContext } from '../../providers/PokemonsProvider';
 
 const PokePedia = () => {
-  const { handleGetRandomPokemon } = useContext(PokemonsContext);
+  const { handleTest } = useContext(PokemonsContext);
+
+  const [pokemon, setPokemons] = useState('pokemon');
+
+  const handleGetRandomPokemon = () => {
+    function getRandomArbitrary(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    let randomPokemon = getRandomArbitrary(1, 898);
+
+    const fetchData = async () => {
+      let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}/`);
+      let response = await res.json();
+      setPokemons(response);
+    };
+    fetchData();
+  };
+
+  const handleShowState = () => {
+    console.log(pokemon);
+  };
 
   return (
     <>
       <StyledGetPokemonWrapper>
         <StyledGetPokemonButton onClick={() => handleGetRandomPokemon()}>Get random pokemon</StyledGetPokemonButton>
-        <StyledPokemonContainer>{<p>Pokedex content</p>}</StyledPokemonContainer>
+        <StyledPokemonContainer>
+          <div>{pokemon.name}</div>
+          <button onClick={() => handleShowState()}>Show pokemon in console</button>
+          <button onClick={() => handleTest()}>Fire provider handle</button>
+        </StyledPokemonContainer>
       </StyledGetPokemonWrapper>
     </>
   );
--- src/providers/PokemonsProvider.js
+++ src/providers/PokemonsProvider.js
@@ -1,41 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export const PokemonsContext = React.createContext({
-  handleGetRandomPokemon: () => {},
+  handleTest: () => {},
 });
 
 const PokemonsProvider = ({ children }) => {
-  const [pokemons, setPokemons] = useState();
-
-  const handleGetRandomPokemon = () => {
-    function getRandomArbitrary(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    }
-
-    let randomPokemon = getRandomArbitrary(1, 898);
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}/`)
-      .then((response) => response.json())
-      .then((data) =>
-        setPokemons(() => {
-          let item = {
-            name: data.name,
-            artwork: data.sprites.other['official-artwork'].front_default,
-            stats: data.stats,
-          };
-          console.log('Your random pokemon: ', item);
-          console.log('previous one (state): ', pokemons);
-          return {
-            item,
-          };
-        })
-      );
+  const handleTest = () => {
+    console.log('function fired from provider');
   };
 
   return (
     <PokemonsContext.Provider
       value={{
-        handleGetRandomPokemon,
+        handleTest,
       }}
     >
       {children}
