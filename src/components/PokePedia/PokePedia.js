a97653a7f698d9b51b507375212a6b83cf10f806
Mankowski92 <mankowski92@gmail.com>
2021-05-26 23:03:42 +0200

    added state in context to further toggle between pokedex option

--- src/components/PokePedia/PokePedia.js
+++ src/components/PokePedia/PokePedia.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyledGetPokemonWrapper } from './PokePedia.styles';
-// import { PokemonsContext } from '../../providers/PokemonsProvider';
+import { PokemonsContext } from '../../providers/PokemonsProvider';
 import RandomPokemon from '../RandomPokemon/RandomPokemon';
 
 const PokePedia = () => {
+  const { handleShowRandomPokemon } = useContext(PokemonsContext);
+
+  const ctx = useContext(PokemonsContext);
+
   return (
     <>
       <StyledGetPokemonWrapper>
-        <RandomPokemon />
+        <button onClick={() => handleShowRandomPokemon()}>Fire provider handle</button>
+        <button onClick={() => console.log('isRandomPokemon state: ', ctx.isRandomPokemon)}>Show randomPokemon State</button>
+        {ctx.isRandomPokemon ? <RandomPokemon /> : 'isRandomPokemon = False'}
+        <div>OTO DANE Z PROVIDERA</div>
       </StyledGetPokemonWrapper>
     </>
   );
--- src/providers/PokemonsProvider.js
+++ src/providers/PokemonsProvider.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const PokemonsContext = React.createContext({
-  handleTest: () => {},
+  handleShowRandomPokemon: () => {},
+  isRandomPokemon: false,
+  isTestComponent: '',
 });
 
 const PokemonsProvider = ({ children }) => {
-  const handleTest = () => {
-    console.log('function fired from provider');
+  const [isRandomPokemon, setRandomPokemon] = useState(false);
+  const [isTestComponent, setTestComponent] = useState(false);
+
+  const handleShowRandomPokemon = () => {
+    console.log('function fired from provider TRUE');
+    setRandomPokemon(true);
   };
 
   return (
     <PokemonsContext.Provider
       value={{
-        handleTest,
+        handleShowRandomPokemon,
+        isRandomPokemon,
+        isTestComponent,
       }}
     >
       {children}
